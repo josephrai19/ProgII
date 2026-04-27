620c2511a2c584a232a83426d50ec775fb62340a
josephrai19 <joseph19.jc@gmail.com>
2026-04-26 21:55:21 -0500

    Laboratorio2

---
+++ Lab2/Lab211.py
@@ -0,0 +1,11 @@
+x = "pyton"
+y = "es"
+z = "facil de aprender"
+print(x, y, z)
+x = "pyton "
+y = "es "
+z = "amigable"
+print(x + y + z)
+x = 5
+y = 10
+print(x + y)
---
+++ Lab2/Lab24.py
@@ -0,0 +1,5 @@
+#Esto es un comentario
+print("hola, mundo")
+print("Hola, Mundo!") #Esto es un comentario
+#print("Hola, Mundo!")
+print("Saludos a todos")
