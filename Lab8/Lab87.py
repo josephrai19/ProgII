62e192d8a56867924ed1c819bdb610ea4c8e5418
josephrai19 <joseph19.jc@gmail.com>
2026-05-20 21:59:30 -0500

    Laboratorio 8

---
+++ Lab8/Lab87.py
@@ -0,0 +1,16 @@
+class Persona:
+    def __init__(self, nom, ape):
+        self.nombre = nom
+        self.apellido = ape
+
+    def imprimir_nombre(self):
+        print(self.nombre, self.apellido)
+
+p1 = Persona("Fulano", "De Tal")
+p1.imprimir_nombre()
+
+class Estudiante(Persona):
+    pass
+
+x = Estudiante("Mengano", "De Tal")
+x.imprimir_nombre()
